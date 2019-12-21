94f3ccd2cb4c9fb807669db596dc0c25b806c6dd
danial <dev.salahi@gmail.com>
2019-12-21 07:41:52 +0330

    finalize the counting sort

--- counting_sort.js
+++ counting_sort.js
@@ -1,16 +1,14 @@
 const range = require("./range");
 const numbers = [1, 2, 2, 2, 1, 3, 3, 1, 2, 4, 5];
-let max = numbers[0];
-for (let i = 0; i < numbers.length; i++) {
-  if (numbers[i] > max) max = numbers[i];
-}
-
-// console.log(Math.max.apply(Math, numbers));
+const max = Math.max.apply(Math, numbers);
 
 let count = Array.apply(null, Array(max + 1)).map(() => 0);
+for (x of numbers)
+    count[x] += 1;
 
-for (x of numbers) {
-  count[x] += 1
-}
-console.log(count);
+let arr = [];
+for (x in range(max + 1))
+  for (i in range(count[x]))
+    arr.push(parseInt([x]));
 
+console.log(arr);
