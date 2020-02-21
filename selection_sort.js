ba0da07c0e47b211e16ba680f1aa506186043fac
danial salahi <dev.salahi@gmail.com>
2020-02-21 17:03:15 +0330

    Wrote the selection sort algo

---
+++ selection_sort.js
@@ -0,0 +1,17 @@
+const selectionSort = arr => {
+    const swap = (arr, idx1, idx2) => ([arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]])
+    for (let i = 0; i < arr.length; i++) {
+        let lowest = i
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[lowest]) {
+                lowest = j
+            }
+        }
+        if (i !== lowest) swap(arr, i, lowest)
+    }
+    return arr
+}
+
+
+let numbers = [1, 0, -5, 10, 11, 25, 3]
+console.log(selectionSort(numbers))
