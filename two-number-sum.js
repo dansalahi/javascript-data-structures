5caf56761edd32fefd90b1e68cb3a0d241287c28
daniel salahi <dan.salahii@gmail.com>
2023-01-17 17:20:37 +0300

    solved two number sum in optimized solution

--- two-number-sum.js
+++ two-number-sum.js
@@ -21,7 +21,33 @@ const twoNumberSum = (array, targetSum) => {
     return result;
 }
 
-console.log(twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10)) // [-1, 11]
-console.log(twoNumberSum([4, 6], 10)) // [4, 6]
+
+// Optimal Solution
+const twoNumberSumOptimal = (array, targetSum) => {
+    if (typeof array !== 'object' || array.length < 2) return []
+    if (typeof targetSum !== 'number') return []
+
+    const sortedArray = array.sort((a, b) => a - b)
+
+    console.log('called')
+    let left = 0
+    let right = sortedArray.length - 1
+    while (left < right) {
+        let currentSum = sortedArray[left] + sortedArray[right];
+        if (currentSum === targetSum)
+            return [sortedArray[left], sortedArray[right]]
+        else if (currentSum < targetSum)
+            left++;
+        else if (currentSum > targetSum)
+            right--;
+    }
+    return []
+
+}
+
+
+
+console.log(twoNumberSumOptimal([3, 5, -4, 8, 11, 1, -1, 6], 10)) // [-1, 11]
+console.log(twoNumberSumOptimal([4, 6], 10)) // [4, 6]
 console.log(twoNumberSum([4, 6, 1], 5)) // [4, 1]
 console.log(twoNumberSum([4, 6, 1, -3], 3)) // [-3, 6]
